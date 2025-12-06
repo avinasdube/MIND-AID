1c41c563bad924e7476c36fbee2d23703f477c36
avinasdube <avidubey712@gmail.com>
2025-12-06 13:30:49 +0530

    Add placeholder script for preparing release artifacts

---
+++ scripts/prepare_release.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Placeholder: bundle artifacts for a release tag.
+# TODO: gather model files, create manifest, checksum.
+
+echo "[WARN] prepare_release.sh not yet implemented." >&2
