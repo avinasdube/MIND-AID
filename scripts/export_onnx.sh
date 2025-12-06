b644fc6b06c8a31ece75b13f78867a9c2deff584
avinasdube <avidubey712@gmail.com>
2025-12-06 13:30:39 +0530

    Add placeholder script for exporting transformer model to ONNX

---
+++ scripts/export_onnx.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Placeholder: export transformer model to ONNX
+# TODO: implement actual export using optimum/transformers.
+
+echo "[WARN] export_onnx.sh not yet implemented." >&2
